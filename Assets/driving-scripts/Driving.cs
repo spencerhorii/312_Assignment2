af02d5cb2a7a000dd9d848dac6c81ea1777f1a7c
Keyaan Vegdani <130519545+KeyaanVegdani@users.noreply.github.com>
2026-07-09 16:07:38 -0700

    Hmmm

--- Assets/driving-scripts/Driving.cs
+++ Assets/driving-scripts/Driving.cs
@@ -3,9 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Driving : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float maxSpeed;
-    [SerializeField] private float acceleration = 10f;
-    [SerializeField] private float deceleration = 40f;   // much higher than acceleration = snappier stop
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
@@ -13,34 +12,18 @@ public class Driving : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        // Clamp so diagonal input doesn't exceed magnitude 1
-        Vector2 clampedInput = Vector2.ClampMagnitude(moveInput, 1f);
-        Vector2 targetVelocity = clampedInput * maxSpeed;
-
-        float rate = clampedInput.sqrMagnitude > 0.01f ? acceleration : deceleration;
-
-        // rb.linearVelocity = Vector2.MoveTowards(
-        //     rb.linearVelocity,
-        //     targetVelocity,
-        //     rate * Time.fixedDeltaTime
-        // );
-
-        if (clampedInput.sqrMagnitude > 0.01f)
-  {
-      rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
-  }
-  else
-  {
-      rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2.zero, deceleration * Time.fixedDeltaTime);
-  }
+        rb.linearVelocity = (moveInput * moveSpeed);
+        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
-};
+}
